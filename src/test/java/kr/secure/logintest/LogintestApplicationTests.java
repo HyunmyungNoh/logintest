c34e0abccc1b72066ce90aa32b17423f6ba3b65b
DESKTOP-EGH6JHP\docker <nhm91@naver.com>
2021-08-03 13:36:39 +0900

    초기 세팅

---
+++ src/test/java/kr/secure/logintest/LogintestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.secure.logintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogintestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
